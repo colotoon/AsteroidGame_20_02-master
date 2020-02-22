c936326aa8c9490180b56323692af6397d3699b3
Валентин <vn20@ya.ru>
2020-02-22 11:14:21 +0300

    Добавьте файлы проекта.

---
+++ AsteroidGame/Game.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AsteroidGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext __Context;
+        private static BufferedGraphics __Buffer;
+
+        public static int Width { get; set; }
+
+        public static int Height { get; set; }
+
+        public static string AsteroidPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\asteroid.png";
+        public static Image AsteroidImage = Image.FromFile(AsteroidPath);
+
+        public static List<asteroid> __asteroids = new List<asteroid>();
+
+        //static Game()
+        //{
+
+        //}
+
+        public static void Initialize(Form form)
+        {
+            Width = form.Width;
+            Height = form.Height;
+
+            __Context = BufferedGraphicsManager.Current;
+            Graphics g = form.CreateGraphics();
+            __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            var timer = new Timer { Interval = 100 };
+            timer.Tick += OnTimerTick;
+            timer.Start();
+        }
+
+        private static void OnTimerTick(object sender, EventArgs e)
+        {
+            Update();
+            Draw();
+        }
+
+        public static void Load()
+        {
+                      
+            Random rnd = new Random();
+            int a_x, a_y, a_s = 0;           
+
+            for (var i = 0; i < 50; i++)
+            {
+                a_s = rnd.Next(1, 5); // размер
+                a_x = rnd.Next(a_s, Width - a_s); // позиция х
+                a_y = rnd.Next(a_s, Height - a_s); // у
+
+                __asteroids.Add(new asteroid(
+                    new Point(a_x, a_y), // позиция
+                    new Point(0, -a_s), // направление(скорость), 
+                                        // больше размер - быстрее летит, эффект 3D 
+                     a_s) // размер
+                     ); 
+            }
+            
+
+        }
+
+        public static void Draw()
+        {
+            var g = __Buffer.Graphics;
+            g.Clear(Color.Black);
+            
+                foreach (var visual_object in __asteroids)
+                visual_object.Draw(g);
+
+            __Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (var visual_object in __asteroids)
+                visual_object.Update();
+        }
+    }
+}
---
+++ AsteroidGame/asteroid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace AsteroidGame
+{
+    class asteroid
+    {
+        protected Point _Position;
+        protected Point _Direction;
+        protected int _Size;
+
+        public asteroid(Point Position, Point Direction, int Size)
+        {
+            _Position = Position;
+            _Direction = Direction;
+            _Size = Size;
+        }
+
+
+        public void Draw(Graphics g)
+        {
+             g.FillEllipse(Brushes.White, new Rectangle(
+                 _Position.X, _Position.Y,
+                 _Size, _Size));
+
+            // с DrawImage нагрузка на проц повышается до 25%
+            //g.DrawImage(Game.AsteroidImage, _Position.X, _Position.Y, _Size, _Size);
+
+
+        }
+
+        public void Update()
+        {
+            _Position.Y -= _Direction.Y;
+            if (_Position.Y <= 0)
+            {
+                
+                _Position.Y = Game.Height;
+            }
+            if(_Position.Y > Game.Height)
+            {
+                _Position.Y = 0;
+            }
+            
+
+        }
+
+    }
+}
